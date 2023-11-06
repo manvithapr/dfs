cbdc21cd4f845372d9314f38f075f2cc1327be2a
manvithapr <126038990+manvithapr@users.noreply.github.com>
2023-11-06 12:47:18 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,34 +1,82 @@
-#include <stdlib.h>
-intsourceV,Vertex,Edge,time,visited[10],Graph[10][10]; 
 
-void DepthFirstSearch(inti) 
-int j; visited[i]=1; 
-printf(" %d->",i++); 
-for(j=0;j<Vertex;j++) 
-{ if(Graph[i][j]==1&&visited[j]==0) DepthFirstSearch(j); 
+      // C code to implement above approach
+#include <stdio.h>
+#include <stdlib.h>
+
+// Globally declared visited array
+int vis[100];
+
+// Graph structure to store number
+// of vertices and edges and
+// Adjacency matrix
+struct Graph {
+	int V;
+	int E;
+	int** Adj;
+};
+
+// Function to input data of graph
+struct Graph* adjMatrix()
+{
+	struct Graph* G = (struct Graph*)
+		malloc(sizeof(struct Graph));
+	if (!G) {
+		printf("Memory Error\n");
+		return NULL;
+	}
+	G->V = 7;
+	G->E = 7;
+
+	G->Adj = (int**)malloc((G->V) * sizeof(int*));
+	for (int k = 0; k < G->V; k++) {
+		G->Adj[k] = (int*)malloc((G->V) * sizeof(int));
+	}
+
+	for (int u = 0; u < G->V; u++) {
+		for (int v = 0; v < G->V; v++) {
+			G->Adj[u][v] = 0;
+		}
+	}
+	G->Adj[0][1] = G->Adj[1][0] = 1;
+	G->Adj[0][2] = G->Adj[2][0] = 1;
+	G->Adj[1][3] = G->Adj[3][1] = 1;
+	G->Adj[1][4] = G->Adj[4][1] = 1;
+	G->Adj[1][5] = G->Adj[5][1] = 1;
+	G->Adj[1][6] = G->Adj[6][1] = 1;
+	G->Adj[6][2] = G->Adj[2][6] = 1;
+
+	return G;
 }
+
+// DFS function to print DFS traversal of graph
+void DFS(struct Graph* G, int u)
+{
+	vis[u] = 1;
+	printf("%d ", u);
+	for (int v = 0; v < G->V; v++) {
+		if (!vis[v] && G->Adj[u][v]) {
+			DFS(G, v);
+		}
+	}
 }
-intmain() { inti,j,vertex1,vertex2; 
-           printf("\t\t\tGraphs\n"); 
-           printf("Enter no. of edges:"); 
-           scanf("%d",&Edge); 
-           printf("Enter no. of vertices:"); 
-           scanf("%d",&Vertex); 
-           for(i=0;i<Vertex;i++)
-             {
-               for(j=0;j<Vertex;j++) Graph[i][j]=0;
-             } 
-           for(i=0;i<Edge;i++)
-             { printf("Enter the edges in V1 V2 : ");
-              scanf("%d%d",&vertex1,&vertex2);
-              Graph[vertex1-1][vertex2-1]=1;
-             } 
-           for(i=0;i<Vertex;i++)
-             { 
-               for(j=0;j<Vertex;j++)
-                 printf(" %d ",Graph[i][j]); printf("\n"); 
-             } printf("Enter source Vertex: "); 
-           scanf("%d",&sourceV); 
-           DepthFirstSearch(sourceV-1);
-           return 0;
-          }
+
+// Function for DFS traversal
+void DFStraversal(struct Graph* G)
+{
+	for (int i = 0; i < 100; i++) {
+		vis[i] = 0;
+	}
+	for (int i = 0; i < G->V; i++) {
+		if (!vis[i]) {
+			DFS(G, i);
+		}
+	}
+}
+
+// Driver code
+void main()
+{
+	struct Graph* G;
+	G = adjMatrix();
+	DFStraversal(G);
+}
